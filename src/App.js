85213a8e151e26b0b03906295fe5d80b83fcd385
lmngoc <lmngoc1011@gmail.com>
2023-07-03 16:25:47 +0700

    multible component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./views/Nav";
 
 //template + logic
 //JSX
 //babel
 const App = () => {
+  let Obj = { name: "Ngoc", channel: "Ngoc channel" }
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world react hook
+          Hello world react hook <br />
+          {JSON.stringify(Obj)}
         </p>
 
       </header>
