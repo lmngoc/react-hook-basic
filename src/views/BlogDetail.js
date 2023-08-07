b3dc21faf34ab9b95516f5dc4a1ad24d40a1f205
lmngoc <lmngoc1011@gmail.com>
2023-08-07 16:24:42 +0700

    reuse hook - blog detai

--- src/views/BlogDetail.js
+++ src/views/BlogDetail.js
@@ -1,11 +1,28 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams, useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "../customize/fetch";
+import "./Blog.scss";
 const BlogDetail = () => {
     let { id } = useParams();
-    console.log("check param id", id);
+    let history = useHistory();
+    //console.log("check param id", id);
+
+    const { data: dataBlogDetail, loading, error } = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    console.log("check datat detail", dataBlogDetail);
+    const handleBackData = () => {
+        history.push("/blog");
+    }
     return (
         <div>
-            Hello detail blog {id}
-        </div>
+            <div><span onClick={handleBackData}>Back</span></div>
+            <div className="blog-detail">
+                {dataBlogDetail &&
+                    <>
+                        <div className="title">Blog ID : {id} --- {loading === true ? "Loading data ... " : dataBlogDetail.title} </div>
+                        <div className="content">{dataBlogDetail.body}</div>
+                    </>
+                }
+            </div>
+        </div >
     )
 }
 export default BlogDetail;
